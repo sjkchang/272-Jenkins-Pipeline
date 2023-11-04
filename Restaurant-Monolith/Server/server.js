6fd5459e2a9aea8d30d3882388bcfcb6c5f43575
sjkchang <sjkchang@gmail.com>
2023-11-04 14:41:29 -0700

    Added jenkins file

--- Restaurant-Monolith/Server/server.js
+++ Restaurant-Monolith/Server/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const sqlite3 = require('sqlite3').verbose();
-const cors = require('cors'); 
+const express = require("express");
+const bodyParser = require("body-parser");
+const sqlite3 = require("sqlite3").verbose();
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
@@ -10,25 +10,27 @@ app.use(cors());
 const PORT = 3001;
 
 // Setup SQLite database
-const db1 = new sqlite3.Database(':memory:', (err) => {
+const db1 = new sqlite3.Database(":memory:", (err) => {
     if (err) {
-        console.error('Failed to create in-memory SQLite database:', err);
+        console.error("Failed to create in-memory SQLite database:", err);
         process.exit(1);
     }
-    console.log('Connected to in-memory SQLite database.');
+    console.log("Connected to in-memory SQLite database.");
 });
 
 // Initialize the database table
 db1.serialize(() => {
-    db.run("CREATE TABLE orders (id INTEGER PRIMARY KEY, item TEXT, quantity INTEGER)");
+    db1.run(
+        "CREATE TABLE orders (id INTEGER PRIMARY KEY, item TEXT, quantity INTEGER)"
+    );
 });
 
 // Middleware to parse JSON requests
 app.use(bodyParser.json());
 
 // Routes
-app.get('/orders', (req, res) => {
-    db.all("SELECT * FROM orders", [], (err, rows) => {
+app.get("/orders", (req, res) => {
+    db1.all("SELECT * FROM orders", [], (err, rows) => {
         if (err) {
             res.status(500).json({ error: err.message });
             return;
@@ -37,44 +39,54 @@ app.get('/orders', (req, res) => {
     });
 });
 
-app.post('/orders', (req, res) => {
+app.post("/orders", (req, res) => {
     const { item, quantity } = req.body;
-    db.run("INSERT INTO orders (item, quantity) VALUES (?, ?)", [item, quantity], function(err) {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
+    db1.run(
+        "INSERT INTO orders (item, quantity) VALUES (?, ?)",
+        [item, quantity],
+        function (err) {
+            if (err) {
+                res.status(500).json({ error: err.message });
+                return;
+            }
+            res.json({ id: this.lastID, item, quantity });
         }
-        res.json({ id: this.lastID, item, quantity });
-    });
+    );
 });
 
-const db2 = new sqlite3.Database(':memory:', (err) => {
+const db2 = new sqlite3.Database(":memory:", (err) => {
     if (err) {
-        console.error('Failed to create in-memory SQLite database:', err);
+        console.error("Failed to create in-memory SQLite database:", err);
         process.exit(1);
     }
-    console.log('Connected to in-memory SQLite database.');
+    console.log("Connected to in-memory SQLite database.");
 });
 
 // Initialize the database table
 db2.serialize(() => {
-    db2.run("CREATE TABLE cooking_tasks (id INTEGER PRIMARY KEY, order_id INTEGER, status TEXT)");
+    db2.run(
+        "CREATE TABLE cooking_tasks (id INTEGER PRIMARY KEY, order_id INTEGER, status TEXT)"
+    );
 });
 
 // Routes
-app.post('/cooking', (req, res) => {
+app.post("/cooking", (req, res) => {
     const { order_id } = req.body;
-    db.run("INSERT INTO cooking_tasks (order_id, status) VALUES (?, ?)", [order_id, "Cooking"], function(err) {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
+    db2.run(
+        "INSERT INTO cooking_tasks (order_id, status) VALUES (?, ?)",
+        [order_id, "Cooking"],
+        function (err) {
+            if (err) {
+                res.status(500).json({ error: err.message });
+                return;
+            }
+            res.json({ id: this.lastID, order_id, status: "Cooking" });
         }
-        res.json({ id: this.lastID, order_id, status: "Cooking" });
-    });
+    );
 });
 
-app.get('/cooking', (req, res) => {
-    db.all("SELECT * FROM cooking_tasks", [], (err, rows) => {
+app.get("/cooking", (req, res) => {
+    db2.all("SELECT * FROM cooking_tasks", [], (err, rows) => {
         if (err) {
             res.status(500).json({ error: err.message });
             return;
@@ -83,32 +95,38 @@ app.get('/cooking', (req, res) => {
     });
 });
 
-const db3 = new sqlite3.Database(':memory:', (err) => {
+const db3 = new sqlite3.Database(":memory:", (err) => {
     if (err) {
-        console.error('Failed to create in-memory SQLite database:', err);
+        console.error("Failed to create in-memory SQLite database:", err);
         process.exit(1);
     }
-    console.log('Connected to in-memory SQLite database.');
+    console.log("Connected to in-memory SQLite database.");
 });
 
 // Initialize the database table
 db3.serialize(() => {
-    db3.run("CREATE TABLE cooking_tasks (id INTEGER PRIMARY KEY, order_id INTEGER, status TEXT)");
+    db3.run(
+        "CREATE TABLE bills (id INTEGER PRIMARY KEY, order_id INTEGER, amount INTEGER)"
+    );
 });
 
-app.post('/billing', (req, res) => {
+app.post("/billing", (req, res) => {
     const { order_id, amount } = req.body;
-    db.run("INSERT INTO bills (order_id, amount) VALUES (?, ?)", [order_id, amount], function(err) {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
+    db3.run(
+        "INSERT INTO bills (order_id, amount) VALUES (?, ?)",
+        [order_id, amount],
+        function (err) {
+            if (err) {
+                res.status(500).json({ error: err.message });
+                return;
+            }
+            res.json({ id: this.lastID, order_id, amount });
         }
-        res.json({ id: this.lastID, order_id, amount });
-    });
+    );
 });
 
-app.get('/billing', (req, res) => {
-    db.all("SELECT * FROM bills", [], (err, rows) => {
+app.get("/billing", (req, res) => {
+    db3.all("SELECT * FROM bills", [], (err, rows) => {
         if (err) {
             res.status(500).json({ error: err.message });
             return;
@@ -116,7 +134,6 @@ app.get('/billing', (req, res) => {
         res.json(rows);
     });
 });
-
 
 // Start the server
 app.listen(PORT, () => {
